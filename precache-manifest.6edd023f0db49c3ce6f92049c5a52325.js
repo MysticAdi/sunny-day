f7796fc7419a67963f7979142fd9a1bcf65dfc4c
MysticAdi <adityapt97@gmail.com>
2019-12-03 12:33:21 +0530

    Updates

---
+++ precache-manifest.6edd023f0db49c3ce6f92049c5a52325.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f1fc07ef606d0acb24851874e721143",
+    "url": "/sunny-day/index.html"
+  },
+  {
+    "revision": "3aedeafd010600217e8e",
+    "url": "/sunny-day/static/css/2.59e78020.chunk.css"
+  },
+  {
+    "revision": "962579d354d67e85d53b",
+    "url": "/sunny-day/static/css/main.063ea1d0.chunk.css"
+  },
+  {
+    "revision": "3aedeafd010600217e8e",
+    "url": "/sunny-day/static/js/2.deb0d015.chunk.js"
+  },
+  {
+    "revision": "962579d354d67e85d53b",
+    "url": "/sunny-day/static/js/main.7a292332.chunk.js"
+  },
+  {
+    "revision": "552655814bc7f952ea34",
+    "url": "/sunny-day/static/js/runtime-main.e09e9d00.js"
+  },
+  {
+    "revision": "ae2847b53a740fb4acdaa481534abcd7",
+    "url": "/sunny-day/static/media/bck.ae2847b5.jpeg"
+  },
+  {
+    "revision": "1cd48d78f06d33973d9d761d426e69bf",
+    "url": "/sunny-day/static/media/weathericons-regular-webfont.1cd48d78.woff2"
+  },
+  {
+    "revision": "4618f0de2a818e7ad3fe880e0b74d04a",
+    "url": "/sunny-day/static/media/weathericons-regular-webfont.4618f0de.ttf"
+  },
+  {
+    "revision": "4b658767da6bd92ce2addb3ce512784d",
+    "url": "/sunny-day/static/media/weathericons-regular-webfont.4b658767.eot"
+  },
+  {
+    "revision": "8cac70ebda3f23ce472110d9f21e8593",
+    "url": "/sunny-day/static/media/weathericons-regular-webfont.8cac70eb.woff"
+  },
+  {
+    "revision": "ecaf8b481729b18f6a8494d9f691cdae",
+    "url": "/sunny-day/static/media/weathericons-regular-webfont.ecaf8b48.svg"
+  }
+]);
